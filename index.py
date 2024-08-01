ed1d49b5e8bd2de4a6c5a427ff47c36649b1494d
ASG <abdulrahman.ghitani02@eng-st.cu.edu.eg>
2024-08-01 19:22:43 +0300

    starter ui

--- index.py
+++ index.py
@@ -19,66 +19,71 @@ class MainApp(QMainWindow, ui):
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.resize(1450, 900)
+        self.tab_buttons_list = [self.operation_btn, self.clinics_btn, self.anesthesia_btn, self.blood_btn, self.IVF_btn]
         self.handel_buttons()
         self.ui_changes()
-
+        self.create_defibrialtor_table()
     def ui_changes(self):
-        self.tabWidget.tabBar().setVisible(False)
+        self.main_tabWidget.tabBar().setVisible(False)
 
     def handel_buttons(self):
         self.close_left_frame.clicked.connect(self.toggle_side_bar)
-        self.pushButton.clicked.connect(self.open_tab_1)
-        self.pushButton_2.clicked.connect(self.open_tab_2)
-        self.pushButton_3.clicked.connect(self.open_tab_3)
-        self.pushButton_4.clicked.connect(self.open_tab_4)
-        self.tabWidget_5.tabCloseRequested.connect(self.close_tab)
+
+        for i, btn in enumerate(self.tab_buttons_list):
+            btn.clicked.connect(lambda _, index=i: self.open_tab(index))
+
+
+
+        self.add_defibrilator_btn.clicked.connect(self.add_row)
+        self.operation_tabWidget.tabCloseRequested.connect(self.close_tab)
 
     def close_tab(self, index):
         print(index)
-        self.tabWidget_5.removeTab(index)
-    # def create_table_of_signals(self):
-    #     self.table_of_signals.setColumnCount(4)
-    #     self.table_of_signals.setHorizontalHeaderLabels(('Frequency', 'Amplitude', 'Shift', ''))
-    #     self.table_of_signals.setColumnWidth(0, 130)
-    #     self.table_of_signals.setColumnWidth(1, 130)
-    #     self.table_of_signals.setColumnWidth(2, 100)
-    #     self.table_of_signals.setColumnWidth(3, 70)
-    #     self.add_row()
-    #
-    # def add_row(self):
-    #     row = self.table_of_signals.rowCount()
-    #     self.table_of_signals.setRowCount(row + 1)
-    #     self.table_of_signals.setRowHeight(row, 30)
-    #
-    #     for col in range(4):  # Add buttons to all columns (0, 1, 2, 3)
-    #         if col == 3:  # For the last column (index 3), add the delete button
-    #             button = QPushButton()
-    #             button.setObjectName(f'delete_btn{row}')
-    #             button.setIcon(QIcon('icons/trash copy.svg'))
-    #             button.setStyleSheet("QPushButton{background-color: rgba(255,255,255,0); border:1px solid rgba(255,255,255,0);} QPushButton:pressed{margin-top:2px }")
-    #             self.table_of_signals.setCellWidget(row, col, button)
-    #             button.clicked.connect(lambda _, row=row: self.delete_row(row))
-    #
-    # def delete_row(self, row):
-    #     if row >= 0 and row < self.table_of_signals.rowCount():
-    #         for col in range(4):  # Remove widgets/items from all columns (0, 1, 2, 3)
-    #             if col == 3:
-    #                 button = self.table_of_signals.cellWidget(row, col)
-    #                 if button is not None:
-    #                     button.deleteLater()
-    #             else:
-    #                 item = self.table_of_signals.item(row, col)
-    #                 if item is not None:
-    #                     item = None
-    #         self.table_of_signals.removeRow(row)
-    #
-    #         # Update the button object names and click connections for the remaining rows
-    #         for i in range(row, self.table_of_signals.rowCount()):
-    #             button = self.table_of_signals.cellWidget(i, 3)
-    #             if button is not None:
-    #                 button.setObjectName(f'delete_btn{i}')
-    #                 button.clicked.disconnect()  # Disconnect previous click connection
-    #                 button.clicked.connect(lambda _, row=i: self.delete_row(row))  # Connect a new click connection
+        self.operation_tabWidget.removeTab(index)
+
+    def create_defibrialtor_table(self):
+        self.defibrilator_table.setColumnCount()
+        self.defibrilator_table.setHorizontalHeaderLabels(('Name', 'Serial', 'Shift', ''))
+        self.defibrilator_table.setColumnWidth(0, 130)
+        self.defibrilator_table.setColumnWidth(1, 130)
+        self.defibrilator_table.setColumnWidth(2, 100)
+        self.defibrilator_table.setColumnWidth(3, 70)
+        self.add_row()
+
+    def add_row(self):
+        row = self.defibrilator_table.rowCount()
+        self.defibrilator_table.setRowCount(row + 1)
+        self.defibrilator_table.setRowHeight(row, 30)
+
+        for col in range(4):  # Add buttons to all columns (0, 1, 2, 3)
+            if col == 3:  # For the last column (index 3), add the delete button
+                button = QPushButton()
+                button.setObjectName(f'delete_btn{row}')
+                button.setIcon(QIcon('icons/trash copy.svg'))
+                button.setStyleSheet("QPushButton{background-color: rgba(255,255,255,0); border:1px solid rgba(255,255,255,0);} QPushButton:pressed{margin-top:2px }")
+                self.defibrilator_table.setCellWidget(row, col, button)
+                button.clicked.connect(lambda _, row=row: self.delete_row(row))
+
+    def delete_row(self, row):
+        if row >= 0 and row < self.defibrilator_table.rowCount():
+            for col in range(4):  # Remove widgets/items from all columns (0, 1, 2, 3)
+                if col == 3:
+                    button = self.defibrilator_table.cellWidget(row, col)
+                    if button is not None:
+                        button.deleteLater()
+                else:
+                    item = self.defibrilator_table.item(row, col)
+                    if item is not None:
+                        item = None
+            self.defibrilator_table.removeRow(row)
+
+            # Update the button object names and click connections for the remaining rows
+            for i in range(row, self.defibrilator_table.rowCount()):
+                button = self.defibrilator_table.cellWidget(i, 3)
+                if button is not None:
+                    button.setObjectName(f'delete_btn{i}')
+                    button.clicked.disconnect()  # Disconnect previous click connection
+                    button.clicked.connect(lambda _, row=i: self.delete_row(row))  # Connect a new click connection
     # In the above code:
     # - We remove the widgets/items from the row that is being deleted, including the delete button.
     # - We then update the button object names and click connections for the remaining rows.
@@ -86,17 +91,9 @@ class MainApp(QMainWindow, ui):
 
 
     ##################################open taps#############################################
-    def open_tab_1(self):
-        self.tabWidget.setCurrentIndex(0)
-
-    def open_tab_2(self):
-        self.tabWidget.setCurrentIndex(1)
-
-    def open_tab_3(self):
-        self.tabWidget.setCurrentIndex(2)
-
-    def open_tab_4(self):
-        self.tabWidget.setCurrentIndex(3)
+    # Define the open_tab function
+    def open_tab(self, tab_index):
+        self.main_tabWidget.setCurrentIndex(tab_index)
 
 
 
