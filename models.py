b4613ea1867d786f8199a60fc35ac18a87f0fc94
Omar Shaban <omar.mohammed021@eng-st.cu.edu.eg>
2024-08-01 23:40:42 +0300

    db & models

---
+++ models.py
@@ -0,0 +1,30 @@
+from peewee import *
+
+database = SqliteDatabase('calibration.db')
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Device(BaseModel):
+    category = TextField()
+    dept_id = TextField()
+    name = TextField(unique=True)
+    serial = TextField(primary_key=True)
+
+    class Meta:
+        table_name = 'device'
+
+class Reading(BaseModel):
+    device_serial = ForeignKeyField(column_name='device_serial', field='serial', model=Device)
+    ref_value = FloatField()
+    value = FloatField()
+    year = UnknownField()  # INTEGER (4)
+
+    class Meta:
+        table_name = 'reading'
+        primary_key = False
+
